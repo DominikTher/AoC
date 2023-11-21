525c584704e3e5cc107745075f2c63bea512bd48
Dominik Ther <ther.dominik@gmail.com>
2023-11-21 10:44:42 +0100

    Refactor

--- src/AoC/AoC.Console/Days/Yr2015/Day4.cs
+++ src/AoC/AoC.Console/Days/Yr2015/Day4.cs
@@ -1,5 +1,6 @@
 ﻿using AoC.Console.Interfaces;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace AoC.Console.Days.Yr2015;
 
@@ -9,36 +10,23 @@ internal class Day4 : IDay
 
     public int DayNumber => 4;
 
-    public object PartOne(IEnumerable<string> rows)
-    {
-        var number = 0;
-        for (;number < int.MaxValue; number++)
-        {
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(rows.First() + number);
-            byte[] hashBytes = MD5.HashData(inputBytes);
-
-            var hash = Convert.ToHexString(hashBytes);
-
-            if (hash.StartsWith("00000"))
-            {
-                break;
-            }
-        }
-
-        return number;
-    }
+    public object PartOne(IEnumerable<string> rows) 
+        => CalculateHash(rows.First(), "00000");
 
     public object PartTwo(IEnumerable<string> rows)
+        => CalculateHash(rows.First(), "000000");
+
+    private static object CalculateHash(string value, string check)
     {
         var number = 0;
         for (; number < int.MaxValue; number++)
         {
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(rows.First() + number);
+            byte[] inputBytes = Encoding.ASCII.GetBytes(value + number);
             byte[] hashBytes = MD5.HashData(inputBytes);
 
             var hash = Convert.ToHexString(hashBytes);
 
-            if (hash.StartsWith("000000"))
+            if (hash.StartsWith(check))
             {
                 break;
             }
