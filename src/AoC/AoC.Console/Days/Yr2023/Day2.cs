05a10ca4d4c0222c908b2670c09afde6f80a4a7e
Dominik Ther <ther.dominik@gmail.com>
2023-12-02 12:14:47 +0100

    2023 Day 2 without refactoring

--- src/AoC/AoC.Console/Days/Yr2023/Day2.cs
+++ src/AoC/AoC.Console/Days/Yr2023/Day2.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Console.Interfaces;
+﻿using AoC.Console.Extensions;
+using AoC.Console.Interfaces;
 
 namespace AoC.Console.Days.Yr2023;
 
@@ -10,11 +11,61 @@ public sealed class Day2 : IDay
 
     public object PartOne(IEnumerable<string> rows)
     {
-        throw new NotImplementedException();
+        var rules = new Dictionary<string, int>
+        {
+            { "red", 12 },
+            { "green", 13 },
+            { "blue", 14 }
+        };
+
+        return rows.WithoutNullOrWhiteSpace()
+            .Select(row =>
+            {
+                var game = row.Split(":");
+                var id = int.Parse(game[0].Split(" ")[1]);
+                var cubes = game[1].Split(";");
+
+                return (id, cubes);
+            })
+            .Where(x => !x.cubes.Any(x =>
+            {
+
+                var sets = x.Split(",");
+                foreach (var set in sets)
+                {
+                    var cube = set.Trim().Split(" ");
+                    (int Count, string Cube) items = (int.Parse(cube[0]), cube[1]);
+
+                    if (rules[items.Cube] < items.Count)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }))
+            .Sum(x => x.id);
     }
 
     public object PartTwo(IEnumerable<string> rows)
-    {
-        throw new NotImplementedException();
-    }
+        => rows.WithoutNullOrWhiteSpace()
+           .Select(row =>
+           {
+               var game = row.Split(":");
+               var id = int.Parse(game[0].Split(" ")[1]);
+
+               return (id, game: game[1]);
+           })
+           .Select(x =>
+           {
+               return x.game.Split(";").SelectMany(a => a.Split(",").Select(b =>
+               {
+                   var cube = b.Trim().Split(" ");
+                   return (int.Parse(cube[0]), cube[1]);
+               }))
+               .GroupBy(x => x.Item2)
+               .Select(g => g.Max(c => c.Item1))
+               .Aggregate(1, (a, b) => a * b);
+           })
+           .Sum();
 }
---
+++ src/AoC/AoC.Console/Days/Yr2023/Day3.cs
@@ -0,0 +1,20 @@
+﻿using AoC.Console.Interfaces;
+
+namespace AoC.Console.Days.Yr2023;
+
+public sealed class Day3 : IDay
+{
+    public int Year => 2023;
+
+    public int DayNumber => 3;
+
+    public object PartOne(IEnumerable<string> rows)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object PartTwo(IEnumerable<string> rows)
+    {
+        throw new NotImplementedException();
+    }
+}
