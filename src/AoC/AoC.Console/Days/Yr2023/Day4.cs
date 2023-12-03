eae7827c3870466f821eb1f9eb033ee0b245eef2
Dominik Ther <dominik.ther@windowslive.com>
2023-12-03 16:59:02 +0100

    Day 4 template

---
+++ src/AoC/AoC.Console/Days/Yr2023/Day4.cs
@@ -0,0 +1,20 @@
+﻿using AoC.Console.Interfaces;
+
+namespace AoC.Console.Days.Yr2023;
+
+public sealed class Day4 : IDay
+{
+    public int Year => 2023;
+
+    public int DayNumber => 4;
+
+    public object PartOne(IEnumerable<string> rows)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object PartTwo(IEnumerable<string> rows)
+    {
+        throw new NotImplementedException();
+    }
+}
