6517c7890ba250f978b88cc05e314a0d726729fa
Dominik Ther <dominik.ther@windowslive.com>
2023-12-04 18:58:52 +0100

    2023 Day 4

--- src/AoC/AoC.Console/Days/Yr2023/Day4.cs
+++ src/AoC/AoC.Console/Days/Yr2023/Day4.cs
@@ -1,4 +1,6 @@
-﻿using AoC.Console.Interfaces;
+﻿using AoC.Console.Extensions;
+using AoC.Console.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace AoC.Console.Days.Yr2023;
 
@@ -9,12 +11,52 @@ public sealed class Day4 : IDay
     public int DayNumber => 4;
 
     public object PartOne(IEnumerable<string> rows)
-    {
-        throw new NotImplementedException();
-    }
+        => rows
+            .WithoutNullOrWhiteSpace()
+            .Select(row =>
+            {
+                var card = row.Split(":");
+                var numbers = card[1].Split("|");
+                var winning = Regex.Matches(numbers[0], @"\d+").Select(m => int.Parse(m.Value));
+                var myNumbers = Regex.Matches(numbers[1], @"\d+").Select(m => int.Parse(m.Value));
+                var myWinning = myNumbers.Intersect(winning).Count();
+
+                return (int)Math.Pow(2, myWinning - 1);
+            })
+            .Sum();
 
     public object PartTwo(IEnumerable<string> rows)
     {
-        throw new NotImplementedException();
+        var instances = new Dictionary<int, int>();
+        foreach (var row in rows.WithoutNullOrWhiteSpace())
+        {
+            var card = row.Split(":");
+            var id = int.Parse(Regex.Match(card[0], @"\d+").Value);
+            var numbers = card[1].Split("|");
+            var winning = Regex.Matches(numbers[0], @"\d+").Select(m => int.Parse(m.Value));
+            var myNumbers = Regex.Matches(numbers[1], @"\d+").Select(m => int.Parse(m.Value));
+            var myWinning = myNumbers.Intersect(winning).Count();
+
+            if (!instances.TryGetValue(id, out var value))
+            {
+                instances.Add(id, 1);
+            }
+            else
+            {
+                instances[id] = value + 1;
+            }
+
+            foreach (var item in Enumerable.Range(id + 1, myWinning))
+            {
+                if (!instances.TryGetValue(item, out value))
+                {
+                    instances.Add(item, 1);
+                }
+
+                instances[item] = value + instances[id];
+            }
+        }
+
+        return instances.Sum(x => x.Value);
     }
 }
