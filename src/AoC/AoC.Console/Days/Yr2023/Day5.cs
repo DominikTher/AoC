15440cdb51e87057302bc2de039c168f2d4bd988
Dominik Ther <dominik.ther@windowslive.com>
2023-12-04 20:56:45 +0100

    Day 5 preparation

---
+++ src/AoC/AoC.Console/Days/Yr2023/Day5.cs
@@ -0,0 +1,20 @@
+﻿using AoC.Console.Interfaces;
+
+namespace AoC.Console.Days.Yr2023;
+
+internal class Day5 : IDay
+{
+    public int Year => 2023;
+
+    public int DayNumber => 5;
+
+    public object PartOne(IEnumerable<string> rows)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object PartTwo(IEnumerable<string> rows)
+    {
+        throw new NotImplementedException();
+    }
+}
